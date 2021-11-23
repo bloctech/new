f5804de533b165823a4c129f219b71552f0f558e
Usman Shabbir <usmanshabbir295@gmail.com>
2021-11-23 17:43:39 +0500

    Theme added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import About from "./components/About";
+import { ThemeProvider } from "@material-ui/styles";
+import { CssBaseline } from "@material-ui/core";
+import theme from "./theme";
 
 function App() {
   return (
     <>
-      <About />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <About />
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/theme/index.js
@@ -0,0 +1,37 @@
+import { createTheme, responsiveFontSizes } from "@material-ui/core/styles";
+
+let theme = createTheme({
+  palette: {
+    common: {
+      white: "#E9FBFA",
+    },
+    primary: {
+      main: "#48007C",
+      light: "#F3F4F7",
+      dark: "#000000",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: "#ffffff",
+      light: "#c9c9c9",
+      contrastText: "#48007C",
+    },
+  },
+});
+theme.overrides = {
+  MuiCssBaseline: {
+    "@global": {
+      body: {
+        backgroundColor: "#ffffff",
+        color: "#000000",
+      },
+      ".img-fluid": {
+        maxWidth: "100%",
+        height: "auto",
+      },
+    },
+  },
+};
+
+theme = responsiveFontSizes(theme);
+export default theme;
